6fc8c51cf791c78ecb1a289cef8aa10204d2b7af
jihyun11 <372491@naver.com>
2024-10-11 12:01:33 +0900

    빈 생명주기 콜백, 빈 스코프

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,33 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    public String logDemo(@RequestBody HttpServletRequest request) {
+
+        // Provider 덕분에 고객 요청이 들어올 때까지 빈의 생성을 지연시킬 수 있다.
+        // 고객 요청이 들어올 "때" myLogger 가 만들어짐.
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
---
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -0,0 +1,21 @@
+package hello.core.web;
+
+import hello.core.annotation.MainDiscountPolicy;
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
